cb312960c8a7322eb23436f2b8c233345cbd6b4e
alvarocabezasdev <45815034+alvarocabezasdev@users.noreply.github.com>
2019-05-23 18:08:56 +0200

    Primer commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyATs_naxuKj1iGyvMRHdTEPrUtfrbc3R0g",
+    authDomain: "pidete-1cf1e.firebaseapp.com",
+    databaseURL: "https://pidete-1cf1e.firebaseio.com",
+    projectId: "pidete-1cf1e",
+    storageBucket: "pidete-1cf1e.appspot.com",
+    messagingSenderId: "522443113973",
+    appId: "1:522443113973:web:70c05daf30858071",
+    productos: "productos",
+    mesa1: "mesa1",
+    mesa2: "mesa2",
+    mesa3: "mesa3",
+    mesa4: "mesa4",
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,33 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+
+  // Your web app's Firebase configuration
+  export const environment = {
+    production: true,
+    firebaseConfig : {
+      apiKey: "AIzaSyATs_naxuKj1iGyvMRHdTEPrUtfrbc3R0g",
+      authDomain: "pidete-1cf1e.firebaseapp.com",
+      databaseURL: "https://pidete-1cf1e.firebaseio.com",
+      projectId: "pidete-1cf1e",
+      storageBucket: "pidete-1cf1e.appspot.com",
+      messagingSenderId: "522443113973",
+      appId: "1:522443113973:web:70c05daf30858071",
+      productos: "productos",
+      mesa1: "mesa1",
+      mesa2: "mesa2",
+      mesa3: "mesa3",
+      mesa4: "mesa4",
+
+    }
+  };
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
